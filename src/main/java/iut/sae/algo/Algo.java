64b94d4738c937a62776d6994a9489c16169595b
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-06-12 15:14:22 +0200

    Ajout méthode rle

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -2,10 +2,20 @@ package iut.sae.algo;
 
 
 public class Algo{
-    public static String RLE(String in){
-        // Provide your algo here
-        return "NotYetImplemented";
+    public static String RLE(String in) {
+        String rle = "";
+        int count = 1;
+        for (int i = 0; i < in.length(); i++) {
+            if (i + 1 < in.length() && in.charAt(i) == in.charAt(i + 1)) {
+                count++;
+            } else {
+                rle += count + "" + in.charAt(i);
+                count = 1;
+            }
+        }
+        return rle;
     }
+    
 
     public static String RLE(String in, int iteration) throws AlgoException{
         // Provide your algo here
